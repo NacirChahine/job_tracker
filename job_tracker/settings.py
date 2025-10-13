a900394afc4f1f1ffad548e1d921a88b78df3c19
Nacir <chahinnacir@gmail.com>
2025-10-12 23:43:00 -0300

    add allowed hosts

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8l9h$!puekl&q4$olmy88#mos!jivb%i&0_jiqg-*leq3+$no3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'narojobtracker.pythonanywhere.com']
 
 
 # Application definition
