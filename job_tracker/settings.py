8e7e8cf1a805f740f25089f7d098ffa27974bc38
Nacir <chahinnacir@gmail.com>
2025-10-12 23:55:00 -0300

    fix edit button

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
